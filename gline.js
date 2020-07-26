adbe66cc97dd32a5ce7ab2b4aeff1975c64ff88d
Bhavik Kothari <bkkothari2255@gmail.com>
2020-07-26 17:35:24 +0530

    gmane email archive analyzing & vizulaizing

---
+++ gline.js
@@ -0,0 +1,3 @@
+gline = [ ['Month','umich.edu','mac.com','unl.edu','mtu.edu','berkeley.edu','virginia.edu','columbia.edu','ucdavis.edu','cam.ac.uk','gmail.com'],
+['2005-12',37,12,12,11,10,8,8,8,7,7]
+];
